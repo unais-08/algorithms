5d82b729f251f384b1d19825d985e1972f95b23c
Unais Shaikh <unais.shaikh.dev@gmail.com>
2025-08-27 18:19:01 +0530

    change naming convention

--- Array/100_Remove_Duplicate_from_Sorted_Array.cpp
+++ Array/100_Remove_Duplicate_from_Sorted_Array.cpp
@@ -1,8 +1,8 @@
-// Time Complexity: O(n)
-// Space Complexity: O(1)
 #include <bits/stdc++.h>
 using namespace std;
 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 class Solution {
    public:
     /**
---
+++ Array/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+   public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (int j = 1; j < nums.size(); ++j) {
+            if (nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+};
