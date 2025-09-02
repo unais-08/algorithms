8871cdd7170c730ecd8e9bf3dd9160402c59d443
Unais Shaikh <unais.shaikh.dev@gmail.com>
2025-09-02 16:54:32 +0530

    brute force for136

--- Array/136_number_appears_once.cpp
+++ Array/136_number_appears_once.cpp
@@ -10,4 +10,24 @@ class Solution {
         }
         return result;
     }
+
+   public:
+    int bruteForce(vector<int>& nums) {
+        map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        for (auto it : mp) {
+            if (it.second == 1) return it.first;
+        }
+        return -1;
+    }
 };
+
+int main() {
+    Solution s;
+    vector<int> v = {4,1,2,1,2};
+    cout << s.bruteForce(v) << endl;
+
+    return 0;
+}
